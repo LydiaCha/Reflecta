0c7c2c0f1c301508b7bd8fd5733d940a73a75365
veronika <hunyakvera@gmail.com>
2022-10-23 22:08:55 +0100

    feat: display reflections

--- client/src/pages/history/history-page.jsx
+++ client/src/pages/history/history-page.jsx
@@ -5,6 +5,7 @@ import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
+import Card from "react-bootstrap/Card";
 
 const HistoryPage = ({ reflections }) => {
   const [reflection, setReflection] = useState();
@@ -23,18 +24,33 @@ const HistoryPage = ({ reflections }) => {
 
   return (
     <Container fluid className="history-page">
-      <Form.Group as={Row} className="md-2">
-        <Col md={{ span: 4 }}>
-          <>
-            <Form.Label>Disabled select menu</Form.Label>
-            <Form.Select onChange={(e) => onSelect(e)}>
-              {reflections.map((reflection, i) => (
-                <option key={i}>{reflection.time}</option>
-              ))}
-            </Form.Select>
-          </>
-        </Col>
-      </Form.Group>
+      <Container>
+        <Form.Group as={Row} className="md-2">
+          <Col md={{ span: 4 }}>
+            <>
+              <Form.Label>Disabled select menu</Form.Label>
+              <Form.Select onChange={(e) => onSelect(e)}>
+                {reflections.map((reflection, i) => (
+                  <option key={i}>{reflection.time}</option>
+                ))}
+              </Form.Select>
+            </>
+          </Col>
+        </Form.Group>
+      </Container>
+      <Container className="reflections">
+        {reflection?.renderedSteps &&
+          reflection.renderedSteps.map((reflection, i) => {
+            return (
+              <Card key={i}>
+                <Card.Header>{reflection.id}</Card.Header>
+                <Card.Body>
+                  <Card.Text>{reflection.message}</Card.Text>
+                </Card.Body>
+              </Card>
+            );
+          })}
+      </Container>
     </Container>
   );
 };
