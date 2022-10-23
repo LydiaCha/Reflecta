7562f3b0c6a72db8a71445b6f3f01633f3a26060
veronika <hunyakvera@gmail.com>
2022-10-23 21:28:35 +0100

    feat: reflection choice

--- client/src/pages/history/history-page.jsx
+++ client/src/pages/history/history-page.jsx
@@ -1,9 +1,40 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
 
 const HistoryPage = ({ reflections }) => {
+  const [reflection, setReflection] = useState();
+
+  useEffect(() => {
+    setReflection(reflections[0]);
+  }, [reflections]);
+
+  const onSelect = (event) => {
+    const time = event.target.value;
+    const currentReflection = reflections.find(
+      (reflection) => reflection.time === time
+    );
+    setReflection(currentReflection);
+  };
+
   return (
     <Container fluid className="history-page">
-      <h1 className="home-page__title"> </h1>
+      <Form.Group as={Row} className="md-2">
+        <Col md={{ span: 4 }}>
+          <>
+            <Form.Label>Disabled select menu</Form.Label>
+            <Form.Select onChange={(e) => onSelect(e)}>
+              {reflections.map((reflection, i) => (
+                <option key={i}>{reflection.time}</option>
+              ))}
+            </Form.Select>
+          </>
+        </Col>
+      </Form.Group>
     </Container>
   );
 };
